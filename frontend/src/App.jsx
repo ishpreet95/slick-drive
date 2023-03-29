30e27c2a86db44453401ff1a60d61d382da1b367
ishpreet95 <ishpreetsingh95@gmail.com>
2023-03-30 01:12:15 +0530

    added login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,45 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+          <div className="container">
+            <Link className="navbar-brand" to={"/sign-in"}>
+              Slick Drive⚡
+            </Link>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-in"}>
+                    Sign In
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={"/sign-up"}>
+                    Sign Up
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Login />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
-
 export default App;
